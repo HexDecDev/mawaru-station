9b092eec5e32982b42021e67df4aed3c030efcf4
HexDec <rinkurosu@gmail.com>
2018-06-04 17:13:16 +0300

    Дроп исправлен на нормальный менеджмент

--- server/app/TracksDatabase.js
+++ server/app/TracksDatabase.js
@@ -15,22 +15,6 @@ const daytracks = mongoose.model('daytracks', Track);
 const nighttracks = mongoose.model('nighttracks', Track);
 const jingles = mongoose.model('jingles', Track);
 
-function RecordTracks(array, collection){
-
-    for (var i = 0; i < array.length; i++){
-        
-        if (array[i]){
-            console.log(`Adding ${array[i]}`);
-            const newTrack = new collection({
-                path:array[i], 
-                playCount: 0,
-                rating: 0
-            })
-            newTrack.save();
-        }
-    }
-
-}
 
 //Можно универсальнее, с конфигами и прочими плюшками.
 //Но с другой стороны это все такой узкоспецилизированный софт, что кому оно кроме меня надо?
@@ -63,15 +47,72 @@ function CollectionsWrapper(folder){
     return returnCollection;
 }
 
+function RecordTracks(array, collection){
+
+    for (var i = 0; i < array.length; i++){
+        
+        if (array[i]){
+            console.log(`Adding ${array[i]}`);
+            const newTrack = new collection({
+                path:array[i], 
+                playCount: 0,
+                rating: 0
+            })
+            newTrack.save();
+        }
+    }
+
+}
+
+function CheckAndAdd(path, collection){
+
+    collection.find({path: path}).then( (data) => {
+        if(!data.length) {
+            console.log(`New track found ${path}`);
+            const newTrack = new collection({
+                path:path, 
+                playCount: 0,
+                rating: 0
+            })
+            newTrack.save();
+        }
+    })
+}
+
+function RemoveTrackFromBase(id, collection){
+    console.log(`File not found. Removing ${id} entry from base`)
+    collection.remove({ _id: id }).then( data => console.log(data));
+}
+
+function UpdateTracks(arrayOfFiles, collection){
+    //Check new tracks in folder
+    for (var iter = 0; iter < arrayOfFiles.length; iter++){
+        
+        if (arrayOfFiles[iter]){
+            CheckAndAdd(arrayOfFiles[iter], collection);
+        }
+    }
+
+    //Check removed files
+    collection.count({}).then( (data) => {
+        for (var i = 0; i < data; i++){
+            collection.findOne({}).skip(i).limit(1).then( (newData) => {
+                if (arrayOfFiles.indexOf(newData.path) === -1)
+                    RemoveTrackFromBase(newData._id, collection)
+            })
+        }
+     })
+    
+
+}
+
 export function GenerateDB(folder, array) {
 
     var collection = CollectionsWrapper(folder);
-    console.log(collection.modelName);
     mongoose.connection.db.listCollections({name: collection.modelName})
     .next(function(err, collinfo) {
         if (collinfo) {
-            console.log('drop');
-            collection.collection.drop().then( () => RecordTracks(array, collection))
+            UpdateTracks(array, collection);
         }
 
         else {
